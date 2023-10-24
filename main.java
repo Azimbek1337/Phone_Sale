57732833f2be3f4cbebfbc56df4ccfc7fb21de27
User <najmiddinturgunboyev08@gmail.com>
2023-10-24 11:50:10 +0500

    SSSSS

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         Methods.first();
         System.out.println(" HELLO WORLD");
+        System.out.println("Kotsan");
 
     }
 }
